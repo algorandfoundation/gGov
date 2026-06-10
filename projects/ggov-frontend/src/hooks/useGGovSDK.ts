0ec1acdc611f1820a45ee0da1169dd6f0352ace3
Tasos Bitsios <tasos.bitsios@algorand.foundation>
2026-06-10 13:45:16 +0200

    fix: frontend: txn validity ~10 minutes, 213 rounds

--- projects/ggov-frontend/src/hooks/useGGovSDK.ts
+++ projects/ggov-frontend/src/hooks/useGGovSDK.ts
@@ -20,7 +20,7 @@ function createAlgorandClient() {
       port: config.port,
       token: config.token,
     },
-  })
+  }).setDefaultValidityWindow(213) // ~10 mins at 2.81 round times
 }
 
 const appId = BigInt(import.meta.env.VITE_GGOV_REGISTRY_APP_ID || '0')
