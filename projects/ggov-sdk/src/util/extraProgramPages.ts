3bd5b79edebd95b3e5403ee60de8a554f3356d1e
Tasos Bitsios <tasos.bitsios@algorand.foundation>
2026-08-24 16:45:43 +0200

    chore: format

--- projects/ggov-sdk/src/util/extraProgramPages.ts
+++ projects/ggov-sdk/src/util/extraProgramPages.ts
@@ -14,6 +14,6 @@ const PROGRAM_PAGE_BYTES = 2048
 export function extraProgramPages(approval: Uint8Array, clear: Uint8Array = new Uint8Array()): number {
   const total = approval.length + clear.length
   const pages = total <= PROGRAM_PAGE_BYTES ? 0 : Math.ceil(total / PROGRAM_PAGE_BYTES) - 1
-  if (pages > 7) throw new Error(`Program too large: needs ${pages} extra program pages (max 7)`) 
+  if (pages > 7) throw new Error(`Program too large: needs ${pages} extra program pages (max 7)`)
   return pages
 }
